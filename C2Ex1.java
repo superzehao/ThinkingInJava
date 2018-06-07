2f8cb67a7da9e24f42e3e8cbc1a16fc7949559f2
superzehao <244116836@qq.com>
2018-06-07 17:40:45 +0800

    第二章习题

---
+++ C2Ex1.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import java.util.Date;
+
+public class C2Ex1 {
+
+
+    //第二章习题
+
+    //ex1，Java只会给类的成员初始化
+
+//    int i;
+//    char c;
+//
+//    public C2Ex1(){
+//        System.out.println("i="+i);
+//        System.out.println("c=["+c+"]");
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        new C2Ex1();
+//    }
+
+
+
+
+}
